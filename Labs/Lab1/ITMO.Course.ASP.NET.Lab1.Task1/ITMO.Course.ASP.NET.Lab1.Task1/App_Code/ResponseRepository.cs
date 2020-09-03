7221d7c2e780f80917e60c1f89ecc92ec2b190ce
EgorAlekseev <54812985+EgorAlekseev@users.noreply.github.com>
2020-09-03 10:27:58 +0300

    Add files

---
+++ Labs/Lab1/ITMO.Course.ASP.NET.Lab1.Task1/ITMO.Course.ASP.NET.Lab1.Task1/App_Code/ResponseRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ITMO.Course.ASP.NET.Lab1.Task1.App_Code
+{
+    public class ResponseRepository
+    {
+        private static ResponseRepository repository = new ResponseRepository();
+        private List<GuestResponse> responses = new List<GuestResponse>();
+        public static ResponseRepository GetRepository()
+        {
+            return repository;
+        }
+        public IEnumerable<GuestResponse> GetAllResponses()
+        {
+            return responses;
+        }
+        public void AddResponse(GuestResponse response)
+        {
+            responses.Add(response);
+        }
+    }
+}
---
+++ Labs/Lab1/ITMO.Course.ASP.NET.Lab1.Task1/ITMO.Course.ASP.NET.Lab1.Task1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ITMO.Course.ASP.NET.Lab1.Task1.Global" Language="C#" %>
