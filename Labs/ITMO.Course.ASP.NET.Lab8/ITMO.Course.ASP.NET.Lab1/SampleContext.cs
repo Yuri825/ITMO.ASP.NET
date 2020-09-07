c729cbd0938a5f1931e63cae5b286737a49a5c0e
EgorAlekseev <54812985+EgorAlekseev@users.noreply.github.com>
2020-09-07 16:34:28 +0300

    Add files

---
+++ Labs/ITMO.Course.ASP.NET.Lab8/ITMO.Course.ASP.NET.Lab1/SampleContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ITMO.Course.ASP.NET.Lab1
+{
+    public class SampleContext : DbContext
+    {
+        public SampleContext() : base("SeminarBD") { }
+        public DbSet<GuestResponse> GuestResponses { get; set; }
+        public DbSet<Report> Reports { get; set; }
+    }
+}
