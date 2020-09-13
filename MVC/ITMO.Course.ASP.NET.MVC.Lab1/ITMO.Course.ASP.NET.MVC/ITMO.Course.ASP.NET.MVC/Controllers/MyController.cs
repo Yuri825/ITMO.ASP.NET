cfd68ad1b949b15d1b6648ea4293e6de861ca172
EgorAlekseev <54812985+EgorAlekseev@users.noreply.github.com>
2020-09-13 20:50:31 +0300

    Add files

---
+++ MVC/ITMO.Course.ASP.NET.MVC.Lab1/ITMO.Course.ASP.NET.MVC/ITMO.Course.ASP.NET.MVC/Controllers/MyController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ITMO.Course.ASP.NET.MVC.Models;
+
+namespace ITMO.Course.ASP.NET.MVC.Controllers
+{
+    public class MyController : Controller
+    {
+        // GET: Home
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public string Index(string hel)
+        {
+            string Greeting = ModelClass.ModelHello() + ", " + hel;
+            return Greeting;
+        }
+    }
+}
---
+++ MVC/ITMO.Course.ASP.NET.MVC.Lab2/ITMO.Course.ASP.NET.MVC/ITMO.Course.ASP.NET.MVC/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ITMO.Course.ASP.NET.MVC.Models;
+
+namespace ITMO.Course.ASP.NET.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public string Index()
+        {
+            //string res = ExeEnum();
+            //string res = ExeStruct();
+            //string res = StudyCsharp.SetStatus(3);
+            //string res = StudyCsharp.ExeSwitch(StudyCsharp.SetStatus(3));
+            //string res = StudyCsharp.GetFunction(0, 9);
+            //string res = ExeFactorial(5);
+            //string res = ExeTriangle();
+            string res = ExeCircle();
+            return res;
+
+
+        }
+
+        public string ExeEnum()
+        {
+            AccountType goldAccount;
+            AccountType platinumAccount;
+
+            goldAccount = AccountType.Checking;
+            platinumAccount = AccountType.Deposit;
+
+            string res1 = String.Format("Тип банковского счета {0}", goldAccount);
+            string res2 = String.Format("Тип банковского счета {0}", platinumAccount);
+            string res = res1 + "<p>" + res2;
+            return res;
+
+
+        }
+
+        public string ExeStruct()
+        {
+            BankAccount goldBankAccount;
+            goldBankAccount.accType = AccountType.Checking;
+            goldBankAccount.accBal = (decimal)3200.00;
+            goldBankAccount.accNo = 123;
+
+            string res = String.Format("Информация о банковском счете: {0}", goldBankAccount);
+           
+            return res;
+        }
+
+        public string ExeFactorial(int x)
+        {
+            int f; 
+            bool ok = StudyCsharp.Factorial(x, out f);
+            if (ok) return String.Format("Факториал числа {0} равен {1} ", x, f); 
+            else return "Невозможно вычислить факториал";
+        }
+
+        public string ExeTriangle()
+        {
+            Triangle tr1 = new Triangle(3, 5, 6);
+            string sq1 = String.Format("Площадь фигуры {0} равна: {1:0.##}", tr1.Name, tr1.Area);
+            return sq1;
+        }
+
+        public string ExeCircle()
+        { 
+            Circle cir1 = new Circle(3); 
+            string sq = String.Format("Площадь фигуры {0} равна: {1:0.##}", cir1.Name, cir1.Area);
+            return sq; 
+        }
+    }
+}
